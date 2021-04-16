31ba92e8c1f0b2852221416c395a7e0b422d677c
fabio carvalho <fabio.carvalho.ext@nokia.com>
2021-04-16 23:00:47 +0100

    factory archetypes apllyed | u+date navigation bar

--- js/menuActive.js
+++ js/menuActive.js
@@ -21,12 +21,49 @@ function servicesActive() {
   document.getElementById("aboutActive").style.color = "rgb(230, 230, 230)";
 }
 
-function updateStageTwo() {
-  $("#logo_black").hide();
-  $("#logo_grey").show();
+function updateStageOne() {
+  sectionOption().firstSection();
+  colorNavigationBar().firstSection();
 }
 
-function updateStageOne() {
-  $("#logo_grey").hide();
-  $("#logo_black").show();
+function updateStageTwo() {
+  sectionOption().secondSection();
+  colorNavigationBar().secondSection();
+}
+
+function sectionOption() {
+  let toggleLogo = new Object();
+  const BLOCK = "block";
+  const NONE = "none";
+  let logoGray = document.querySelector("#logo_grey");
+  let logoBlack = document.querySelector("#logo_black");
+
+  toggleLogo.firstSection = function () {
+    logoGray.style.display = NONE;
+    logoBlack.style.display = BLOCK;
+  };
+
+  toggleLogo.secondSection = function () {
+    logoGray.style.display = BLOCK;
+    logoBlack.style.display = NONE;
+  };
+  return toggleLogo;
+}
+
+function colorNavigationBar() {
+  let section = new Object();
+  const navigationBarElements = document.querySelector(".navigation ul").children;
+  const isNavigationElementsRendered = () => navigationBarElements.length !== 0;
+
+  section.firstSection = function () {
+    if (!isNavigationElementsRendered()) return;
+    navigationBarElements[0].classList.add("navigation-section-one");
+    navigationBarElements[0].classList.remove("navigation-section-one-not-selected");
+  };
+  section.secondSection = function () {
+    if (!isNavigationElementsRendered()) return;
+    navigationBarElements[1].classList.add("navigation-section-two");
+    navigationBarElements[0].classList.add("navigation-section-one-not-selected");
+  };
+  return section;
 }
